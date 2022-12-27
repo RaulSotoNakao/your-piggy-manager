e31fa822cafa79fdadf40bcd1d0ee708b9725604
raulito <raul_0.4_@hotmail.com>
2022-12-27 17:42:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'your-piggy-manager',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
