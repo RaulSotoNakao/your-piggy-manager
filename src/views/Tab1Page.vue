a0e76950d40a35aa5d1c634816b62e6012969095
raulito <raul_0.4_@hotmail.com>
2023-01-01 16:17:54 +0100

    my first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,19 +11,39 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+      <input v-model="input" />
+      <button @click="() => updateOption()">click</button>
+      <button @click="() => recover()">recover first</button>
+      <div></div>
+      {{ [...array.entries()] }}
+
+      <div v-for="el in array" :key="el">{{ el.name }}</div>
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script setup>
+import { ref, reactive, isReactive, isRef, computed, toRef, unref } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+const count = ref(0);
+const input = ref(null);
+let array = ref([{ name: "help" }, { name: "me" }, { name: "men" }]);
+
+const recover = () => {
+  input.value = array.value[0].name;
+};
+
+const updateOption = () => {
+  array.value = [...unref(array), { name: input }];
+  console.log(array.value[array.value.length - 1]);
+};
 </script>
